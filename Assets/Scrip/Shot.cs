1fd231d14e7fe7a85f71b5093f727e849b95f47e
AzulGrajales <azul.grajales35@my.unitec.edu.mx>
2023-06-22 21:19:53 -0600

    Bullets arregladas

--- Assets/Scrip/Shot.cs
+++ Assets/Scrip/Shot.cs
@@ -8,18 +8,24 @@ public class Shot : MonoBehaviour
     public GameObject bullet;
 
     public float shotForce = 1500f;
-    public float shortRate = 0.5f;
+    public float shotRate = 0.5f;
+    private float shotRateTime = 0;
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
-            GameObject newBullet;
+           
+                GameObject newBullet;
 
-            newBullet = Instantiate(bullet,spawnPoint.position, spawnPoint.rotation);
+                newBullet = Instantiate(bullet, spawnPoint.position, spawnPoint.rotation);
 
-            newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+                newBullet.GetComponent<Rigidbody>().AddForce(spawnPoint.forward * shotForce);
+
+                Destroy(newBullet, 1);
+           
+            
         }
     }
 }
